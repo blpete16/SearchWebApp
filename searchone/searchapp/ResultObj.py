31ab2a013ee0f06a66226a098f8bbf003d58b36e
Brian Peterson <bpete16@lsu.edu>
2015-10-31 17:26:25 -0500

    changed query stuff to work off of real database

--- searchone/searchapp/ResultObj.py
+++ searchone/searchapp/ResultObj.py
@@ -1,5 +1,6 @@
 
 
 class ResultObj():
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, row):
+        #zero is url, one is author
+        self.text = row[0] + ", " + row[1]
